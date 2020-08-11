2b5f3b7c1e00e5dd9a710552dc5c90b57c8cf1c9
roh8raut <roh8raut@gmail.com>
2020-08-11 13:40:49 +0530

    Updates

---
+++ precache-manifest.a0a22ad56b1c0932fac1f82f35f415b8.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e342794eb3b4a83fa84d2662f514ec1",
+    "url": "/glams/index.html"
+  },
+  {
+    "revision": "6e1ca08f32f048bbaa43",
+    "url": "/glams/static/css/main.c511ad43.chunk.css"
+  },
+  {
+    "revision": "08f8002adb008d70e30e",
+    "url": "/glams/static/js/2.4386994e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/glams/static/js/2.4386994e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6e1ca08f32f048bbaa43",
+    "url": "/glams/static/js/main.8fbe0c53.chunk.js"
+  },
+  {
+    "revision": "8d01e6d60757431f01d5",
+    "url": "/glams/static/js/runtime-main.be556c4d.js"
+  },
+  {
+    "revision": "86ae56df00bc76c1eac8bb00836b9902",
+    "url": "/glams/static/media/Glams_Privacy_Policy.86ae56df.pdf"
+  },
+  {
+    "revision": "dcfedfed38a8fc936e98a65c4bbfa6f8",
+    "url": "/glams/static/media/Glams_Terms.dcfedfed.pdf"
+  },
+  {
+    "revision": "87ab5a929a46b809a5736286652a8606",
+    "url": "/glams/static/media/logo.87ab5a92.svg"
+  }
+]);
--- src/utility.js
+++
@@ -1,9 +0,0 @@
-export const isEmailValid = (value) => {
-  const emailRegex = new RegExp(
-    /^([a-zA-Z0-9_\-.]+)@([a-zA-Z\-.]{3,10})\.([a-zA-Z]{2,5})$/
-  );
-  if (emailRegex.test(value)) {
-    return true;
-  }
-  return false;
-};
