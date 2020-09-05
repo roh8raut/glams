5ec330101e4aee92033cb2fdc77dde1fc6567768
roh8raut <roh8raut@gmail.com>
2020-09-05 17:10:41 +0530

    Updates

---
+++ precache-manifest.94b9c139fcfb61fe549332da4d7af2c9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bfd214996d5c5b2629c7ac7c5440c057",
+    "url": "/glams/index.html"
+  },
+  {
+    "revision": "f82bdb792ecdb40f3cfe",
+    "url": "/glams/static/css/main.87633ab5.chunk.css"
+  },
+  {
+    "revision": "682551dac658a1399ddb",
+    "url": "/glams/static/js/2.67ae5304.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/glams/static/js/2.67ae5304.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f82bdb792ecdb40f3cfe",
+    "url": "/glams/static/js/main.6021e0a0.chunk.js"
+  },
+  {
+    "revision": "8d01e6d60757431f01d5",
+    "url": "/glams/static/js/runtime-main.be556c4d.js"
+  },
+  {
+    "revision": "87ab5a929a46b809a5736286652a8606",
+    "url": "/glams/static/media/logo.87ab5a92.svg"
+  }
+]);
